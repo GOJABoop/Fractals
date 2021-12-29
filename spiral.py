0b90293148bfa80cd3b61b1313f7f02f0e3ad1b3
Alberto Garcia <josealbertogarcia453@gmail.com>
2021-12-28 18:23:48 -0600

    Spiral fractal

---
+++ spiral.py
@@ -0,0 +1,18 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor("black")
+t.width(2)
+t.speed(10)
+t.hideturtle()
+
+colors = ('white', 'pink', 'cyan')
+
+for i in range (300):
+    t.pencolor(colors[i % 3])
+    t.forward(i * 4)
+    t.right(121)
+
+input()
